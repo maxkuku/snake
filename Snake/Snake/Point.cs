f4eff6e7a2f00b16247a7c5ab2a4d562fd325ea3
Maksim <maxkuku@gmail.com>
2023-01-15 15:20:35 +0300

    Инкапсуляция
    Работа класса изолирована от пользователя класса

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -11,6 +11,23 @@ namespace Snake
 		public int y;
 		public char sym;
 
+        
+        public Point()
+        {
+            // это конструктор который писать не обязательно
+            // он ничего не возвращает и возвращаемое
+            // значение мы не указываем совсем
+        }
+
+        public Point(int _x, int _y, char _sym)
+        {
+            // заполнение конструктора, чтобы в программе уже написать
+            // Point p1 = new Point(1,3,'*');
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,10 +11,10 @@ class Program
         static void Main(string[] args)
         {
 
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            Point p1 = new Point(1,3,'*');
+            //p1.x = 1;
+            //p1.y = 3;
+            //p1.sym = '*';
 
             p1.Draw();
 
