b8b2f470a8626b103b2a68249d3aced40d009ef4
Maksim <maxkuku@gmail.com>
2023-01-15 16:56:10 +0300

    Списки и Цикл

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -12,19 +12,39 @@ class Program
         {
 
             Point p1 = new Point(1,3,'*');
-            //p1.x = 1;
-            //p1.y = 3;
-            //p1.sym = '*';
-
             p1.Draw();
 
 
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
-
+            Point p2 = new Point(4,5,'#');
             p2.Draw();
+
+
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            numList.RemoveAt(0);
+
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
+
 
             Console.ReadLine();
         }
